fc2096a14b44de0900a34568912ad10d98f34f65
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-25 12:49:56 +0000

    fix: Fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-// import Image from "next/image";
 import Sidebar from "@components/ui/sidebar/Sidebar";
 import MobileMenu from "@components/ui/navigation/mobile-menu/MobileMenu";
 import Nav from "@components/ui/navigation/desktop-nav/Nav";
 
 export default function Home() {
   const [show, setShow] = useState<boolean>(false);
-  // const [profile, setProfile] = useState(false);
-  // const [menu, setMenu] = useState(false);
 
   function toggleMenu() {
     setShow(!show);
@@ -18,9 +15,9 @@ export default function Home() {
       <div className="w-full h-full bg-gray-200">
         <div className="flex flex-no-wrap">
           <Sidebar />
-          <MobileMenu onToggle={toggleMenu} IsShown={show} />
+          <MobileMenu onToggle={toggleMenu} isShown={show} />
           <div className="w-full">
-            <Nav onToggle={toggleMenu} IsShown={show} />
+            <Nav onToggle={toggleMenu} isShown={show} />
             <main className="w-full h-[calc(100vh-64px)] bg-red-100">MAP</main>
           </div>
         </div>
