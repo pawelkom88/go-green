63796374eafafa85f137fe5c8abd1cb9c66ce3b4
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-25 10:55:23 +0000

    chore: Change path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import "@fontsource/odibee-sans"
 import "@fontsource/nunito"
-import '../styles/globals.css'
+import '@styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
