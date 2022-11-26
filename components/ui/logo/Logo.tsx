77c5b00ca5809509d0cc099ba9ba499719ee4ce8
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-26 11:14:27 +0000

    refactor: Remove padding and center logo

--- components/ui/logo/Logo.tsx
+++ components/ui/logo/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="min-h-max w-full flex items-center px-8 pt-2 ">
+    <div className="min-h-max w-full flex items-center justify-center">
       <Image src={"/assets/logo.png"} width={150} height={150} alt="d" />
     </div>
   );
