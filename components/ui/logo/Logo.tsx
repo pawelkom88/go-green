d52f2e2ca33b99687f1e27b699d32789a93254bd
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-27 12:35:34 +0000

    chore: Change styles

--- components/ui/logo/Logo.tsx
+++ components/ui/logo/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="min-h-max w-full flex items-center justify-center">
+    <div className="min-h-max w-full flex-center">
       <Image src={"/assets/logo.png"} width={150} height={150} alt="d" />
     </div>
   );
