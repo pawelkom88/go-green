66c70ea22e9aa51c5246da01705e89e625aa9df1
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-27 12:50:18 +0000

    refactor: Refactor Nav

--- components/ui/navigation/desktop-nav/Nav.tsx
+++ components/ui/navigation/desktop-nav/Nav.tsx
@@ -1,74 +1,39 @@
-import { useState } from "react";
-import Image from "next/image";
+import UserMenu from "@components/user-menu/UserMenu";
 import SearchIcon from "@components/ui/icons/SearchIcon";
-import ProfileIcon from "@components/ui/icons/ProfileIcon";
-import HamburgerIcon from "@components/ui/icons/HamburgerIcon";
-import SignOutIcon from "@components/ui/icons/SignOutIcon";
-import Chevron from "@components/ui/icons/Chevron";
-import Avatar from "@components/ui/avatar/Avatar";
-
-type MobileMenuProps = {
-  isShown: boolean;
-  onToggle: (state: boolean) => void;
-};
+import { MobileMenuProps } from "@types/types";
 
 export default function Nav({ isShown, onToggle }: MobileMenuProps) {
-  const [profile, setProfile] = useState(false);
-
+  console.log("render Nav");
   return (
-    <nav className="h-16 flex items-center lg:items-stretch justify-end lg:justify-between bg-white shadow relative z-10">
-      <div className="hidden lg:flex w-full pr-6">
-        <div className="w-1/2 h-full hidden lg:flex items-center pl-6 pr-24">
-          <div className="relative w-full">
+    <nav className="h-max flex items-center lg:items-stretch justify-end lg:justify-between bg-grey-100 shadow relative py-2 z-10">
+      <div className="lg:flex w-full">
+        <div className="w-full h-full flex-center flex-wrap lg:pr-24">
+          <h1 className="text-xl lg:text-3xl tracking-wide mx-12 py-2">
+            Electric Car Charging Point Finder
+          </h1>
+          <div className="relative w-3/4 md:w-2/3 lg:w-1/3 flex-center">
             <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
+              {/* change to navigation icon */}
               <SearchIcon />
             </div>
             <input
               className="border border-gray-100 focus:outline-none focus:border-indigo-700 rounded w-full text-sm text-gray-500 bg-gray-100 pl-12 py-2"
               type="text"
-              placeholder="Search"
+              placeholder="Postcode"
             />
+            <button title="Navigation button" aria-label="Locate me" className="w-15 h-full  px-2">
+              <SearchIcon />
+            </button>
+
+            <button
+              title="Navigation button"
+              aria-label="Locate me"
+              className="lg:hidden w-15 h-full  px-2">
+              <SearchIcon />
+            </button>
           </div>
         </div>
-        <div className="w-1/2 hidden lg:flex">
-          <div className="w-full flex items-center pl-8 justify-end">
-            <div
-              className="flex items-center relative cursor-pointer"
-              onClick={() => setProfile(!profile)}>
-              <div className="rounded-full">
-                {profile ? (
-                  <ul className="p-2 w-full border-r bg-white absolute rounded left-0 shadow mt-12 sm:mt-16 ">
-                    <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center">
-                      <div className="flex items-center">
-                        <ProfileIcon />
-                        <span className="text-sm ml-2">My Profile</span>
-                      </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center mt-2">
-                      <div className="flex items-center">
-                        <SignOutIcon />
-                        <span className="text-sm ml-2">Sign out</span>
-                      </div>
-                    </li>
-                  </ul>
-                ) : (
-                  ""
-                )}
-                <div className="mx-3">
-                  <Avatar name="asd" />
-                </div>
-              </div>
-              <div className="cursor-pointer text-gray-600">
-                <Chevron />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        className="text-gray-600 mr-8 visible lg:hidden relative"
-        onClick={() => onToggle(!isShown)}>
-        {isShown ? " " : <HamburgerIcon />}
+        <UserMenu />
       </div>
     </nav>
   );
