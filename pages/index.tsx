965c32213cd8c7e2ecf1994cd6f24a2d765ab0dd
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-27 12:33:38 +0000

    chore: Change component composition for better performance ( still may change)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,17 @@
-import { useState } from "react";
 // import Image from "next/image";
 import Sidebar from "@components/ui/sidebar/Sidebar";
-import MobileMenu from "@components/ui/navigation/mobile-menu/MobileMenu";
-import Nav from "@components/ui/navigation/desktop-nav/Nav";
+import Main from "@components/main/Main";
+import Map from "@components/map/Map";
 
 export default function Home() {
-  const [show, setShow] = useState<boolean>(false);
-  // const [profile, setProfile] = useState(false);
-  // const [menu, setMenu] = useState(false);
-
-  function toggleMenu() {
-    setShow(!show);
-  }
-
   return (
     <>
-      <div className="w-full h-full bg-gray-200">
+      <div className="w-full bg-gray-200">
         <div className="flex flex-no-wrap">
-          <Sidebar />
-          <MobileMenu onToggle={toggleMenu} IsShown={show} />
-          <div className="w-full">
-            <Nav onToggle={toggleMenu} IsShown={show} />
-            <main className="w-full h-[calc(100vh-64px)] bg-red-100">MAP</main>
-          </div>
+          <Sidebar/>
+          <Main>
+            <Map/>
+          </Main>
         </div>
       </div>
     </>
