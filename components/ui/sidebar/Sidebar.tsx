91f5896a631b683ffb62771cb31f7a1454ff0612
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-27 12:47:32 +0000

    refactor: Refactor SideBar

--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -1,21 +1,42 @@
-import SidebarItem from "./SidebarItem";
-// import React, { ReactNode } from "react";
-import Image from "next/image";
+import { useState } from "react";
+import Filter from "@components/filters/Filters";
+import HamburgerIcon from "../icons/HamburgerIcon";
+import CloseBtn from "@components/ui/icons/CloseBtn";
 import Logo from "@components/ui/logo/Logo";
 
-// type ChildrenProps = {
-//   children: ReactNode;
-// };
-
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <>
-      <aside className="absolute lg:relative w-80 h-screen shadow bg-gray-100 hidden lg:block">
-        <Logo />
-        <ul className="flex flex-col justify-center items-center py-6">
-          <SidebarItem name="sidebar item" />
-          <SidebarItem name="sidebar item" />
-          <SidebarItem name="sidebar item" />
+      <aside className={isOpen ? "w-80 sidebar" : "w-20 sidebar"}>
+        <ul className="relative h-full flex-center flex-col py-6">
+          {isOpen ? (
+            <>
+              <li
+                className="absolute top-4 right-4 cursor-pointer z-40"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}>
+                <CloseBtn />
+              </li>
+              <li className="absolute top-2 left-0 right-0 m-auto">
+                <Logo />
+              </li>
+              <li>
+                <Filter />
+              </li>
+            </>
+          ) : (
+            <li className="h-full flex flex-col justify-center items-center">
+              <span
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}>
+                <HamburgerIcon />
+              </span>
+            </li>
+          )}
         </ul>
       </aside>
     </>
