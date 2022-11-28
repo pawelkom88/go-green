0e334a8804e4a7c85a7ae7eb4ba0c2aeee0ebb85
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-28 20:53:48 +0000

    chore: Increase size of the user menu and styles

--- components/user-menu-options/UserMenuOptions.tsx
+++ components/user-menu-options/UserMenuOptions.tsx
@@ -2,18 +2,19 @@ import ProfileIcon from "@components/ui/icons/ProfileIcon";
 import SignOutIcon from "@components/ui/icons/SignOutIcon";
 
 export default function UserMenuOptions({}) {
+  console.log("UserMenuOptions renders");
   return (
-    <ul className="p-2 w-full border-r bg-white absolute rounded left-0 shadow mt-12 sm:mt-16 ">
-      <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center">
+    <ul className="p-4 w-full border-r bg-primary-clr absolute rounded left-0 shadow mt-12 sm:mt-16 ">
+      <li className="flex w-full justify-between hover:text-secondary-clr cursor-pointer items-center">
         <div className="flex items-center">
           <ProfileIcon />
-          <span className="text-sm ml-2">My Profile</span>
+          <span className="text-md ml-2">My Profile</span>
         </div>
       </li>
-      <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center mt-2">
+      <li className="flex w-full justify-between hover:text-secondary-clr  cursor-pointer items-center mt-2">
         <div className="flex items-center">
           <SignOutIcon />
-          <span className="text-sm ml-2">Sign out</span>
+          <span className="text-md ml-2">Sign out</span>
         </div>
       </li>
     </ul>
