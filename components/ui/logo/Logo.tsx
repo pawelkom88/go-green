76490d494d1bfec2b5cde8f91b74d24acc89458d
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-28 20:52:10 +0000

    chore: Change size of the logo

--- components/ui/logo/Logo.tsx
+++ components/ui/logo/Logo.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function Logo() {
+  console.log("Logo renders");
+
   return (
     <div className="min-h-max w-full flex-center">
-      <Image src={"/assets/logo.png"} width={150} height={150} alt="d" />
+      <Image src={"/assets/logo.png"} width={180} height={180} alt="App logo" />
     </div>
   );
 }
