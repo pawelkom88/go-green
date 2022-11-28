d1ec623d308f9362f8cc956fb3bec3e6cbfed23b
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-28 20:59:03 +0000

    chore: Change background on the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import Nav from "@components/ui/navigation/Nav";
 import Map from "@components/map/Map";
 
 export default function Home() {
+  console.log("Home renders");
   return (
     <>
-      <div className="w-full bg-gray-200">
+      <div className="w-full bg-primary-clr">
         <div className="flex flex-no-wrap">
           <Sidebar />
           <main className="w-full">
