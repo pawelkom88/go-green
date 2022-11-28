39572163dc66de0de8d61d204d52903c4aa1952c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-28 20:52:47 +0000

    chore: Change tag name

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 export default function Map() {
-  console.log("render");
-  return <main className="w-full h-[calc(100vh-64px)] bg-red-100">MAP</main>;
+  console.log("MAp renders");
+  return <div className="w-full h-[calc(100vh-64px)] bg-white">MAP</div>;
 }
