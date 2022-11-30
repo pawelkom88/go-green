c2f13d54859b7e60186571e6234b65dd5d6ca993
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-30 17:29:57 +0000

    feat: Add types file

--- types/types.ts
+++ types/types.ts
@@ -1,1 +1,19 @@
-export {}
+// USE GEOLOCATION HOOK TYPES
+export interface GeolocationType {
+    code: number;
+    message: string;
+  }
+  
+  export interface PositionType {
+    coords: {
+      latitude: number;
+      longitude: number;
+    };
+    timestamp: number;
+  }
+  
+  export type GeolocationState = {
+    lat: number;
+    lng: number;
+  };
+  
