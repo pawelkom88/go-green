f6207b19fbfc41f6696523cea688e48ffd951bc8
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-02 18:58:41 +0000

    chore: Add Overlay, CloseBtn and displayed msg prop to modal component

--- components/ui/modal/Modal.tsx
+++ components/ui/modal/Modal.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CloseBtn from "../icons/CloseBtnIcon";
+import Overlay from "@components/ui/modal/Overlay";
 
-export default function Modal({className}) {
+type ModalTypes = {
+  onError: string;
+};
+
+export default function Modal({ onError }: ModalTypes) {
+  const [modalClose, setModalClose] = useState(false);
+
+  function closeModal() {
+    setModalClose(true);
+  }
+
+  /*
+  set modal state to default after every change in onError state
+*/
+  useEffect(() => setModalClose(false), [onError]);
+
   return (
-    <dialog className={className} open>
-      <p>Greetings, one and all!</p>
-      <button>OK</button>
-    </dialog>
+    <>
+      {!modalClose && (
+        <Overlay modalClose={modalClose}>
+          <dialog
+            className="min-h-[20vh] overflow-hidden rounded-lg bg-white shadow-xl sm:my-8 sm:w-full sm:max-w-lg text-center text-lg md:text-xl"
+            open>
+            <CloseBtn onClose={closeModal} className="absolute top-4 right-4" />
+            <p className="mt-14">{onError}</p>
+          </dialog>
+        </Overlay>
+      )}
+    </>
   );
 }
