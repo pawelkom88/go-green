6b6371d75906e880119439061a36beb72d86fca2
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-02 23:39:48 +0000

    chore: Edit types

--- types/types.ts
+++ types/types.ts
@@ -1,19 +1,26 @@
+// UNIVERSAL TYPES
+export type ChildrenType = {
+  children: React.ReactNode;
+};
+
 // USE GEOLOCATION HOOK TYPES
 export interface GeolocationType {
-    code: number;
-    message: string;
-  }
-  
-  export interface PositionType {
-    coords: {
-      latitude: number;
-      longitude: number;
-    };
-    timestamp: number;
-  }
-  
-  export type GeolocationState = {
-    lat: number;
-    lng: number;
+  code: number;
+  message: string;
+  onError?: {
+    status: string;
   };
-  
+}
+
+export interface PositionType {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+  timestamp: number;
+}
+
+export type GeolocationState = {
+  lat: number;
+  lng: number;
+};
