c603f920b5fc533288cebfd565a5c647c55dc3ab
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-08 13:27:12 +0000

    chore: Add info window for each location and radius circle

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,55 +1,101 @@
-import React, { useState } from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, useLoadScript, MarkerF, InfoWindow, Circle } from "@react-google-maps/api";
 import Modal from "@components/ui/modal/Modal";
-import { containerStyle, londonCoords, data } from "@helpers/helpers";
+import { containerStyle, londonCoords } from "@helpers/helpers";
 import { UserLocationProps } from "types/types";
 
-export default function Map({ userLocation }: UserLocationProps) {
-  const [isOpen, setIsOpen] = useState(false);
+type propertiesType = {
+  id: number;
+  address: {
+    lat: number;
+    lng: number;
+  };
+};
 
+const options = {
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: "#FF0000",
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 3000,
+  zIndex: 1,
+};
+
+export default function Map({ userLocation, data, radius }: UserLocationProps) {
+  const [selectedPoint, setSelectedPoint] = useState(null);
   const { isLoaded, loadError } = useLoadScript({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_API_KEY as string,
   });
 
+  const center = userLocation ? userLocation : londonCoords;
+
+  // useEffect(() => {
+  //   const listener = e => {
+  //     if (e.key === "Escape") {
+  //       setSelectedPoint(null);
+  //     }
+  //   };
+  //   window.addEventListener("keydown", listener);
+  //   return () => {
+  //     window.removeEventListener("keydown", listener);
+  //   };
+  // }, []);
+
   return (
-    <div className="w-full h-[calc(100vh-64px)] bg-white">
+    <div
+      className="w-full 
+    h-[calc(100vh-84px-48px)] 
+    md:h-[calc(100vh-60px-48px)] 
+    lg:h-[calc(100vh-60px)] bg-white">
       {isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          zoom={14}
-          center={userLocation ? userLocation : londonCoords}>
+        <GoogleMap mapContainerStyle={containerStyle} zoom={14} center={center}>
+          <Circle center={center} options={options} />
           {/* 
                 user position - default to London coords 
           */}
-          <MarkerF
-            icon={"/assets/electric-car.svg"}
-            position={userLocation ? userLocation : londonCoords}
-          />
+          <MarkerF icon={"/assets/electric-car.svg"} position={center} />
           {/* 
                 charging points position - displayed after user action 
           */}
-          {data.map(({ ID, Latitude: nlat, Longitude: nlng }) => {
-            const lat = nlat + (Math.random() * (0.002));
-            const lng = nlng + (Math.random() * (0.01));
-   
+
+          {data?.map(chargingPoint => {
             return (
               <MarkerF
-                key={ID}
+                key={chargingPoint.id}
                 icon={"/assets/charger-station.svg"}
-                position={{ lat, lng }}
-                onClick={() => setIsOpen(!isOpen)}
+                position={{ ...chargingPoint.address }}
+                onClick={() => setSelectedPoint(chargingPoint)}
               />
             );
           })}
 
+          {selectedPoint && (
+            <InfoWindow
+              onCloseClick={() => {
+                setSelectedPoint(null);
+              }}
+              position={{
+                lat: selectedPoint?.address.lat,
+                lng: selectedPoint?.address.lng,
+              }}>
+              <div>
+                <h3 className="relative text-cyan-900 text-lg p-4 z-40">
+                  {selectedPoint.address.title}
+                </h3>
+              </div>
+            </InfoWindow>
+          )}
         </GoogleMap>
       ) : (
         "Loading"
       )}
       {loadError && <Modal onError="Map cannot be displayed" />}
-      {isOpen && <Modal onError="Map cannot be displayed" />}
     </div>
   );
 }
-
