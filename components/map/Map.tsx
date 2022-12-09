a5bc6256f3a544ac3ba7f67f49da0f0747d6fca4
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-09 21:20:13 +0000

    feat: Add direction between two coords

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { GoogleMap, useLoadScript, MarkerF, InfoWindow } from "@react-google-maps/api";
+import React, { useState } from "react";
+import ChargingPointInfo from "@features/google-map/ChargingPointInfo";
+import Marker from "@features/google-map/Marker";
 import Modal from "@components/ui/modal/Modal";
+import { GoogleMap, useLoadScript, MarkerF, DirectionsRenderer } from "@react-google-maps/api";
 import { containerStyle, londonCoords } from "@helpers/helpers";
 import { Coords, DataType } from "types/types";
 
-type PropsType = {
+type MapPropsType = {
   userLocation: undefined | Coords;
   data: Array<DataType>;
   radius: number;
@@ -12,11 +14,11 @@ type PropsType = {
 
 type LatLngLiteral = google.maps.LatLngLiteral;
 type DirectionsResult = google.maps.DirectionsResult;
-type MapOptions = google.maps.MapOptions;
+// type MapOptions = google.maps.MapOptions;
 
-export default function Map({ userLocation, data, radius }: PropsType) {
+export default function Map({ userLocation, data, radius }: MapPropsType) {
   const [selectedPoint, setSelectedPoint] = useState<null | DataType>(null);
-  console.log(selectedPoint);
+  const [direction, setDirections] = useState<null | DirectionsResult>(null);
   const { isLoaded, loadError } = useLoadScript({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_API_KEY as string,
@@ -24,60 +26,42 @@ export default function Map({ userLocation, data, radius }: PropsType) {
 
   const center = userLocation ? userLocation : londonCoords;
 
-  // useEffect(() => {
-  //   const listener = e => {
-  //     if (e.key === "Escape") {
-  //       setSelectedPoint(null);
-  //     }
-  //   };
-  //   window.addEventListener("keydown", listener);
-  //   return () => {
-  //     window.removeEventListener("keydown", listener);
-  //   };
-  // }, []);
-
   return (
-    <div
-      className="w-full 
-    h-[calc(100vh-84px-48px)] 
-    md:h-[calc(100vh-60px-48px)] 
-    lg:h-[calc(100vh-60px)] bg-white">
+    <div className="w-full h-[calc(100vh-84px-48px)] md:h-[cal(100vh-60px-48px)] lg:h-[calc(100vh-60px)] bg-white">
       {isLoaded ? (
         <GoogleMap mapContainerStyle={containerStyle} zoom={14} center={center as LatLngLiteral}>
           {/* 
-                user position - default to London coords 
-              */}
+            user position - default to London coords 
+          */}
           <MarkerF icon={"/assets/electric-car.svg"} position={center as LatLngLiteral} />
           {/* 
-                charging points position - displayed after user action 
-              */}
-
-          {data?.map(chargingPoint => {
-            return (
-              <MarkerF
-                key={chargingPoint.id}
-                icon={"/assets/charger-station.svg"}
-                position={{ ...chargingPoint.address }}
-                onClick={() => setSelectedPoint(chargingPoint)}
-              />
-            );
-          })}
-
+            charging points position - displayed after user action and direction
+          */}
+          <Marker
+            userLocation={userLocation}
+            onSetSelectedPoint={setSelectedPoint}
+            onSetDirection={setDirections}
+            data={data}
+          />
+          {/* 
+            charging points info 
+          */}
           {selectedPoint && (
-            <InfoWindow
-              onCloseClick={() => {
-                setSelectedPoint(null);
+            <ChargingPointInfo selectedPoint={selectedPoint} onCloseClick={setSelectedPoint} />
+          )}
+
+          {direction && (
+            <DirectionsRenderer
+              directions={direction}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#164a41",
+                  strokeWeight: 5,
+                },
+                suppressMarkers: true,
               }}
-              position={{
-                lat: selectedPoint?.address.lat,
-                lng: selectedPoint?.address.lng,
-              }}>
-              <div>
-                <h3 className="relative text-cyan-900 text-lg p-4 z-40">
-                  {selectedPoint.address.title}
-                </h3>
-              </div>
-            </InfoWindow>
+            />
           )}
         </GoogleMap>
       ) : (
