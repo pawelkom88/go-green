19779c63d049993ea737eebc529ad73b62518c52
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-10 19:19:27 +0000

    chore: Add TickIcon component

---
+++ components/ui/icons/TickIcon.tsx
@@ -0,0 +1,18 @@
+export default function TickIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={30}
+      height={30}
+      className="w-5 h5 "
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#05182c"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M5 12l5 5l10 -10" />
+    </svg>
+  );
+}
