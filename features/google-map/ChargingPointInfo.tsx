85631533d8f2c5d9bb5c472d869853436d4b8119
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-12 18:45:04 +0000

    chore: Create link to google maps with user location and charging point destination

--- features/google-map/ChargingPointInfo.tsx
+++ features/google-map/ChargingPointInfo.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Button from "@components/ui/button/Button";
 import { InfoWindow } from "@react-google-maps/api";
-import { DataType } from "types/types";
+import { DataType, Coords } from "types/types";
 
 type ChargingPointInfoType = {
   selectedPoint: DataType;
+  userLocation: undefined | Coords;
   onCloseClick: (val: null) => void;
   onShowDetails: (val: boolean) => void;
 };
 
 export default function ChargingPointInfo({
+  userLocation,
   selectedPoint,
   onCloseClick,
   onShowDetails,
 }: ChargingPointInfoType) {
-  function handler() {
-    console.log("direction?.routes[0].legs[0]");
-  }
+  const { lat, lng } = userLocation || {};
+
+  const chargingPoint = {
+    lat: selectedPoint?.address.lat,
+    lng: selectedPoint?.address.lng,
+  };
+
+  const directionLink = `https://www.google.com/maps/dir/?api=1&origin=${lat},${lng}&destination=${chargingPoint.lat},${chargingPoint.lng}&travelmode=driving`;
 
   return (
     <InfoWindow
@@ -24,10 +31,7 @@ export default function ChargingPointInfo({
         onCloseClick(null);
         () => onShowDetails(false);
       }}
-      position={{
-        lat: selectedPoint?.address.lat,
-        lng: selectedPoint?.address.lng,
-      }}>
+      position={chargingPoint}>
       <div className="w-full mx-auto text-primary-clr text-center py-24 flex flex-col">
         <h3 className="font-bold text-lg my-2">{selectedPoint.address.title}</h3>
         <span>{selectedPoint.address.postCode}</span>
@@ -37,11 +41,13 @@ export default function ChargingPointInfo({
             className="bg-secondary-clr my-2 mr-2 py-2 px-4 text-primary-clr font-bold">
             Details
           </Button>
-          <Button
-            onClick={handler}
+          <a
+            target="_blank"
+            href={directionLink}
+            rel="noopener noreferrer"
             className="bg-primary-clr my-2 py-2 px-4 text-secondary-clr font-bold">
             Get direction
-          </Button>
+          </a>
         </div>
       </div>
     </InfoWindow>
