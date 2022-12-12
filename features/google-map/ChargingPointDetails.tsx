529b727a1a3180618dccc1c27c7f937591735c29
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-12-12 18:44:41 +0000

    chore: Add and display distance and duration

--- features/google-map/ChargingPointDetails.tsx
+++ features/google-map/ChargingPointDetails.tsx
@@ -1,6 +1,6 @@
 import { DataType } from "types/types";
 import TickIcon from "@components/ui/icons/TickIcon";
-import CloseBtnIcon from "@components/ui/icons/CloseBtnIcon";
+// import CloseBtnIcon from "@components/ui/icons/CloseBtnIcon";
 import NoIcon from "@components/ui/icons/NoIcon";
 
 type DirectionsResult = google.maps.DirectionsResult;
@@ -17,7 +17,7 @@ export default function ChargingPointDetails({
   onShowDetails,
 }: chargingPointDetailsType) {
   const { connection, paymentOptions, info } = chargingPointDetails || {};
-  const { distance, duration, start_location, end_location } = direction?.routes[0].legs[0] || {};
+  const { distance, duration } = direction?.routes[0].legs[0] || {};
   const AvailabilityIcons = paymentOptions?.IsAccessKeyRequired ? (
     <TickIcon strokeClr="#f1b24a" />
   ) : (
@@ -26,49 +26,46 @@ export default function ChargingPointDetails({
 
   const noInfo = "Not available";
 
-  console.log(direction?.routes[0].legs[0]);
-
   return (
-    <>
-      <table className="absolute left-0 right-0 mx-0 text-md bg-primary-clr border-4 border-teriary-clr z-50">
-        <CloseBtnIcon className="absolute right-2 top-2" onClose={() => onShowDetails(false)} />
-        <thead className="bg-white text-primary-clr">
-          <tr>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Connection type</th>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Payment options</th>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Info</th>
-          </tr>
-        </thead>
+    <table className="absolute left-48 top-4 text-md bg-primary-clr border-4 border-teriary-clr z-40">
+      {/* <CloseBtnIcon className="absolute right-2 top-2" onClose={() => onShowDetails(false)} /> */}
+      <thead className="bg-white text-primary-clr">
+        <tr>
+          <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Connection type</th>
+          <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Payment options</th>
+          <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Info</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          <tr>
-            <td className="whitespace-nowrap px-4 py-2 font-medium flex-center flex-col">
-              <span className="mr-8"> {connection?.currentType}</span>
-              <span>{connection?.connectionType}</span>
-            </td>
-            <td className="whitespace-nowrap px-4 py-2">
-              <span className="flex-center">Membership required: {AvailabilityIcons}</span>
-              <span className="flex-center">Access Key required: {AvailabilityIcons}</span>
-              <span className="flex-center">Pay at location: {AvailabilityIcons}</span>
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 flex-center flex-col">
-              <span>
-                <a href={`mailto:${info?.eMail}`}>{info?.eMail || noInfo}</a>
-              </span>
-              <span>
-                <a href={`tel:${info?.phone}`}>{info?.phone || noInfo}</a>
-              </span>
-              <span>
-                <a href={info?.website}>{info?.website || noInfo}</a>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>{distance?.text}</td>
-            <td>{duration?.text}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+      <tbody>
+        <tr>
+          <td className="whitespace-nowrap px-4 py-2 font-medium flex-center flex-col">
+            <span className="mr-8"> {connection?.currentType}</span>
+            <span>{connection?.connectionType}</span>
+          </td>
+          <td className="whitespace-nowrap px-4 py-2">
+            <span className="flex-center">Membership required: {AvailabilityIcons}</span>
+            <span className="flex-center">Access Key required: {AvailabilityIcons}</span>
+            <span className="flex-center">Pay at location: {AvailabilityIcons}</span>
+          </td>
+          <td className="whitespace-nowrap px-4 py-2 flex-center flex-col">
+            <span>
+              <a href={`mailto:${info?.eMail}`}>{info?.eMail || noInfo}</a>
+            </span>
+            <span>
+              <a href={`tel:${info?.phone}`}>{info?.phone || noInfo}</a>
+            </span>
+            <span>
+              <a href={info?.website}>{info?.website || noInfo}</a>
+            </span>
+          </td>
+        </tr>
+        <tr className="bg-white text-primary-clr font-bold">
+          <td className="py-2 px-2">Distance: {distance?.text}</td>
+          <td></td>
+          <td className="py-2 px-2">Estimate Travel Time: {duration?.text}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
