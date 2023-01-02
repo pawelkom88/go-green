1e8982be4dacf06041e2252f9c5c7589756b41a3
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-02 12:09:42 +0000

    refactor: Refactor Nav component

--- components/ui/navigation/Nav.tsx
+++ components/ui/navigation/Nav.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import UserLocation from "@components/user-location/UserLocation";
+import PostCodeValidation from "@features/post-code-validation/PostCodeValidation";
 import UserMenu from "@components/user-menu/UserMenu";
 import Filters from "@components/filters/Filters";
+import FilterIcon from "@components/ui/icons/FilterIcon";
+import LocationIcon from "@components/ui/icons/LocationIcon";
 
 export type UserLocationProps = {
   onLocateUser: (val: object) => void;
@@ -10,15 +12,22 @@ export type UserLocationProps = {
 export default function Nav({ onLocateUser }: UserLocationProps) {
   const [isFilterMenuOpen, setIsFilterMenuOpen] = useState<boolean>(false);
 
-  function toggleFiltersMenu() {
-    setIsFilterMenuOpen(!isFilterMenuOpen);
-  }
-
   return (
     <>
       <nav className="h-max relative py-2 z-10">
-        <div className="lg:flex w-full">
-          <UserLocation onLocateUser={onLocateUser} onToggleFiltersMenu={toggleFiltersMenu} />
+        <div className="h-full flex justify-evenly lg:justify-center items-center flex-wrap pr-4 lg:ml-24">
+          <h1 className="text-md sm:text-md xl:text-xl text-secondary-clr tracking-wide uppercase font-bold md:mr-6 py-2">
+            Find the nearest charging point
+          </h1>
+          <PostCodeValidation />
+          <div onClick={onLocateUser} className="h-full ml-2 px-2">
+            <LocationIcon />
+          </div>
+          <div
+            onClick={() => setIsFilterMenuOpen(!isFilterMenuOpen)}
+            className="lg:hidden h-full px-2">
+            <FilterIcon />
+          </div>
           <UserMenu />
         </div>
       </nav>
