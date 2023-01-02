9a25496b1a68e79af464f74179ea68d9c2c9cccf
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-02 10:19:11 +0000

    refactor: Refactor useBoundingBox hook / wrap it in useEffect hook

--- hooks/useBoundingBox.tsx
+++ hooks/useBoundingBox.tsx
@@ -1,22 +1,25 @@
+import { useEffect, useState } from "react";
 import * as turf from "@turf/turf";
 import { userLocation, BoundingBox } from "types/types";
 
 export default function useBoundingBox(currentLocation: userLocation, radius: number) {
-  let boundingBoxPolygon: BoundingBox;
+  const [boundingBoxPolygon, setBoundingBoxPolygon] = useState<BoundingBox>(null);
 
-  if (currentLocation) {
+  useEffect(() => {
+    if (!currentLocation) return;
+
     const { lat, lng } = currentLocation;
 
     /*
- Create a buffer (radius = distance in miles) around the given lat and lng and get the bounding box of the buffer.
-*/
+      Create a buffer (radius = distance in miles) around the given lat and lng and get the bounding box of the buffer.
+      */
 
     const p = turf?.point([lat, lng]);
     const buffer = turf?.buffer(p, radius, { units: "miles" });
     const bbox = turf?.bbox(buffer);
     const polygon = turf?.bboxPolygon(bbox);
-    boundingBoxPolygon = polygon.bbox;
-  }
+    setBoundingBoxPolygon(polygon.bbox);
+  }, [currentLocation, radius]);
 
   return { boundingBoxPolygon };
 }
