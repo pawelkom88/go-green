1f8153ce71fd3730a1e1efd2e23ae350e53186c2
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-03 12:15:17 +0000

    chore: Pass down setRadius setter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 // hooks
-import {useState} from "react";
+import { useState } from "react";
 import useGeolocation from "@hooks/useGeolocation";
 
 // components
 import MobileMenu from "@components/mobile-menu/MobileMenu";
-import Sidebar from "@components/ui/sidebar/Sidebar";
-import Nav from "@components/ui/navigation/Nav";
+import Sidebar from "@components/sidebar/Sidebar";
+import Nav from "@components/navigation/Nav";
 import MapData from "@components/map-data/MapData";
 import Modal from "@components/ui/modal/Modal";
 
 export default function Home() {
-  const [radius, setRadius] = useState(5)
+  const [radius, setRadius] = useState(5);
 
   const { currentLocation, getCurrentPosition, status } = useGeolocation();
   return (
@@ -19,14 +19,14 @@ export default function Home() {
         <div className="flex flex-no-wrap">
           <Sidebar />
           <main className="w-full">
-            <Nav onLocateUser={getCurrentPosition} />
-            <MapData userLocation={currentLocation} radius={radius}/>
+            <Nav onLocateUser={getCurrentPosition} onRadiusChange={setRadius}/>
+            <MapData userLocation={currentLocation} radius={radius} />
             <MobileMenu />
           </main>
         </div>
       </div>
 
-      {status.length !== 0 && <Modal info={status} />}
+      {status.length !== 0 && <Modal>{status}</Modal>}
     </>
   );
 }
