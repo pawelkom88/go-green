ab61c87f2f0fbb6638385cecee2bd48910476144
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-04 12:39:14 +0000

    refactor: Refactor ChargingPointDetails

--- features/google-map/ChargingPointDetails.tsx
+++ features/google-map/ChargingPointDetails.tsx
@@ -1,72 +1,37 @@
-import { DataType } from "types/types";
-import TickIcon from "@components/ui/icons/TickIcon";
-import NoIcon from "@components/ui/icons/NoIcon";
+import { useState } from "react";
+import LocationDetails from "@components/location-details/LocationDetails";
+import Comments from "@components/location-details/comments/Comments";
+import Photos from "@components/location-details/photos/Photos";
 import Modal from "@components/ui/modal/Modal";
+import Tabs from "@components/ui/tabs/Tabs";
+import { chargingPointDetails } from "types/types";
+import { chargingPointTabName } from "helpers/helpers";
 
-type DirectionsResult = google.maps.DirectionsResult;
-
-type chargingPointDetailsType = {
-  chargingPointDetails: DataType;
-  direction: null | DirectionsResult;
+interface chargingPointDetailsType extends chargingPointDetails {
   onShowDetails?: (val: boolean) => void;
-};
+}
 
 export default function ChargingPointDetails({
   direction,
   chargingPointDetails,
   onShowDetails,
 }: chargingPointDetailsType) {
-  const { connection, paymentOptions, info } = chargingPointDetails || {};
-  const { distance, duration } = direction?.routes[0].legs[0] || {};
-  const AvailabilityIcons = paymentOptions?.IsAccessKeyRequired ? (
-    <TickIcon strokeClr="#f1b24a" />
-  ) : (
-    <NoIcon />
-  );
+  const [activeTab, setActiveTab] = useState("Details");
 
-  const noInfo = "Not available";
+  const [details, comments, photos] = chargingPointTabName;
 
   return (
     <Modal onShowDetails={onShowDetails}>
-      <table className="absolute left-48 top-4 text-md bg-primary-clr border-4 border-teriary-clr z-40">
-        <thead className="bg-white text-primary-clr">
-          <tr>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Connection type</th>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Payment options</th>
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium">Info</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td className="whitespace-nowrap px-4 py-2 font-medium flex-center flex-col">
-              <span className="mr-8"> {connection?.currentType}</span>
-              <span>{connection?.connectionType}</span>
-            </td>
-            <td className="whitespace-nowrap px-4 py-2">
-              <span className="flex-center">Membership required: {AvailabilityIcons}</span>
-              <span className="flex-center">Access Key required: {AvailabilityIcons}</span>
-              <span className="flex-center">Pay at location: {AvailabilityIcons}</span>
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 flex-center flex-col">
-              <span>
-                <a href={`mailto:${info?.eMail}`}>{info?.eMail || noInfo}</a>
-              </span>
-              <span>
-                <a href={`tel:${info?.phone}`}>{info?.phone || noInfo}</a>
-              </span>
-              <span>
-                <a href={info?.website}>{info?.website || noInfo}</a>
-              </span>
-            </td>
-          </tr>
-          <tr className="bg-white text-primary-clr font-bold">
-            <td className="py-2 px-2">Distance: {distance?.text}</td>
-            <td></td>
-            <td className="py-2 px-2">Estimate Travel Time: {duration?.text}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Tabs onActiveTab={setActiveTab} activeTab={activeTab} />
+      {activeTab === details.tabName && (
+        <LocationDetails
+          details={details.tabName}
+          direction={direction}
+          chargingPointDetails={chargingPointDetails}
+        />
+      )}
+      {activeTab === comments.tabName && <Comments details={comments.tabName} />}
+      {activeTab === photos.tabName && <Photos details={photos.tabName} />}
     </Modal>
   );
 }
