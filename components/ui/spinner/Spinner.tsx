41c01de81753e50cc484d59273cc71efe92cfbec
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-10 12:48:31 +0000

    feat: Add new icons used in Comments component

---
+++ components/ui/spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-primary-clr"></div>
+  );
+}
