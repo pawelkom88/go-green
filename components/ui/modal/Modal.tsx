0e8086b7fcabb4191960e34ecce8d847d620cb06
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-10 19:08:47 +0000

    chore: Add new prop to Modal component

--- components/ui/modal/Modal.tsx
+++ components/ui/modal/Modal.tsx
@@ -7,9 +7,10 @@ import FocusLock from "react-focus-lock";
 type ModalTypes = {
   children?: React.ReactNode;
   onShowDetails?: (val: boolean) => void;
+  size: string;
 };
 
-export default function Modal({ children, onShowDetails }: ModalTypes) {
+export default function Modal({ children, onShowDetails, size }: ModalTypes) {
   const [isOpen, setIsopen] = useState(true);
 
   function closeModal() {
@@ -24,7 +25,7 @@ export default function Modal({ children, onShowDetails }: ModalTypes) {
           <Overlay onClose={closeModal}>
             <dialog
               onClick={e => e.stopPropagation()}
-              className="sm:h-full md:h-[845px] overflow-y-auto rounded-lg bg-white shadow-xl sm:my-8 pt-8 sm:w-full sm:max-w-2xl text-lg md:text-xl"
+              className={`${size} overflow-y-auto lg:rounded-lg bg-white shadow-xl sm:my-8 pt-8 md:max-w-3xl text-lg md:text-xl`}
               open>
               <CloseBtnIcon onClose={closeModal} className="absolute top-4 right-4" />
               {children}
