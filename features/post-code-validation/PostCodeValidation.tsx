d5820429c9d59ef4b881bfdbea6e07e066504ba7
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-11 20:55:22 +0000

    chore: Implement Input component

--- features/post-code-validation/PostCodeValidation.tsx
+++ features/post-code-validation/PostCodeValidation.tsx
@@ -1,3 +1,4 @@
+import Input from "@components/ui/input-field/Input";
 import Button from "@components/ui/button/Button";
 import { useState } from "react";
 
@@ -5,26 +6,30 @@ const submitBtnStyles =
   "bg-teriary-clr py-2 px-4 text-black font-bold hover:bg-white hover:text-primary-clr";
 
 export default function PostCodeValidation({}) {
-  const [x, setX] = useState("");
-
-  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
-    setX(e.target.value);
-  }
-
+  const [input, setInput] = useState("");
+  
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log("submit");
   }
 
+  function handleInput(e: Event) {
+    setInput((e.target as HTMLInputElement).value);
+  }
+
   return (
     <form onSubmit={handleSubmit} className="flex-center">
-      <input
+      <Input
         onChange={handleInput}
-        className="border-2 md:border-4 border-teriary-clr focus:outline-none focus:border-secondary-cls rounded text-sm text-black md:pl-12 py-2 mr-2"
+        value={input}
+        srOnly={true}
+        id="poscode"
+        name="postcode"
         type="text"
         placeholder="Postcode"
-        value={x}
-      />
+        className="border-2 md:border-4 border-teriary-clr focus:outline-none focus:border-secondary-cls rounded text-sm text-black md:pl-12 py-2 mr-2">
+        Postcode
+      </Input>
       <Button type="submit" className={submitBtnStyles}>
         Submit
       </Button>
