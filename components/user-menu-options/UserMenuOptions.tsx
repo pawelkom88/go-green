1d482bc8be8e2d3c52cf4a0d5cc4acd3d90950fd
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-12 21:12:18 +0000

    chore: Wrap icons with Button component for better accessibility

--- components/user-menu-options/UserMenuOptions.tsx
+++ components/user-menu-options/UserMenuOptions.tsx
@@ -7,13 +7,13 @@ export default function UserMenuOptions({}) {
     <ul className="p-4 w-full border-r bg-primary-clr absolute rounded left-0 shadow mt-12 sm:mt-16 ">
       <li className="flex w-full justify-between hover:text-secondary-clr cursor-pointer items-center">
         <div className="flex items-center">
-          <ProfileIcon />
+          <ProfileIcon size={25} fill="none" />
           <Button className="text-md ml-2">My Profile</Button>
         </div>
       </li>
       <li className="flex w-full justify-between hover:text-secondary-clr  cursor-pointer items-center mt-2">
         <div className="flex items-center">
-          <SignOutIcon />
+          <SignOutIcon size={25} fill="none" />
           <Button className="text-md ml-2">Sign out</Button>
         </div>
       </li>
