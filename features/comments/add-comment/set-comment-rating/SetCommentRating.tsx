3cbc0f4c4b0112ab07a717f2e5d55f8d198678f8
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-15 11:30:14 +0000

    feat: Add SetCommentRating component

---
+++ features/comments/add-comment/set-comment-rating/SetCommentRating.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Button from "@components/ui/button/Button";
+
+type SetCommentRatingProps = {
+  numberOfStars: number;
+  index: number;
+  onSet: (val: number) => void;
+};
+
+export default function SetCommentRating({ numberOfStars, onSet, index }: SetCommentRatingProps) {
+    
+  function handleRating(e: React.MouseEvent<HTMLElement>) {
+    const target = e.target as HTMLElement;
+    onSet(Number(target.dataset.value));
+  }
+  return (
+    <Button onClick={() => onSet(index + 1)}>
+      <Image
+        width={25}
+        height={25}
+        src={index < (numberOfStars as number) ? "/assets/star.svg" : "/assets/empty-star.svg"}
+        className="cursor-pointer"
+        key={index}
+        data-value={index + 1}
+        onMouseOver={handleRating}
+        alt="star"
+      />
+    </Button>
+  );
+}
