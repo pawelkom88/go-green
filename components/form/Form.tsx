a0a4954b8f623d0e3e5d2347ca472e6a83d4217d
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-03-26 22:24:49 +0100

    refactor: Move state up and refactor

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -3,21 +3,23 @@ import ClearInputFieldIcon from "@components/ui/icons/ClearInputFieldIcon";
 import HidePasswordIcon from "@components/ui/icons/HidePasswordIcon";
 import ShowPasswordIcon from "@components/ui/icons/ShowPasswordIcon";
 import Input from "@components/ui/input-field/Input";
-import { loginBtnStyles, signInBtnStyles } from "domain/constants";
-import { FormProps, UserDetails } from "domain/types";
+import { formActions, loginBtnStyles, signInBtnStyles } from "domain/constants";
+import { FormProps } from "domain/types";
 import { useState } from "react";
 
-export default function Form({ children, action, onSubmit }: FormProps) {
-  const [userDetails, setUserDetails] = useState<UserDetails>({
-    userName: "",
-    password: "",
-  });
+export default function Form({
+  children,
+  action,
+  onSubmit,
+  userDetails,
+  onHandleUserDetails,
+}: FormProps) {
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   function handleUserDetails(e: Event) {
     const { name, value } = e.target as HTMLInputElement;
 
-    setUserDetails({
+    onHandleUserDetails({
       ...userDetails,
       [name]: value,
     });
@@ -28,16 +30,16 @@ export default function Form({ children, action, onSubmit }: FormProps) {
   ) : (
     <ShowPasswordIcon size={28} />
   );
-
+console.log(userDetails)
   return (
     <form onSubmit={onSubmit} className="relative space-y-6">
       <div className="space-y-1 text-sm">
         <Input
           onChange={handleUserDetails}
-          value={userDetails.userName}
+          value={userDetails.email}
           srOnly={false}
           type="email"
-          name="userName"
+          name="email"
           id="userName"
           placeholder="john@gmail.com"
           className={`${loginBtnStyles}`}
@@ -50,7 +52,7 @@ export default function Form({ children, action, onSubmit }: FormProps) {
           onChange={handleUserDetails}
           value={userDetails.password}
           srOnly={false}
-          type={showPassword ? "text" : "password"}
+          type="password"
           name="password"
           id="password"
           placeholder="*************"
@@ -64,13 +66,13 @@ export default function Form({ children, action, onSubmit }: FormProps) {
           {isPasswordVisible}
         </Button>
         {userDetails.password.length > 0 && (
-          <Button onClick={() => setUserDetails({ ...userDetails, password: "" })}>
+          <Button onClick={() => onHandleUserDetails({ ...userDetails, password: "" })}>
             <ClearInputFieldIcon className="absolute top-[1.7rem] right-[2.6rem] z-50" size={25} />
           </Button>
         )}
       </div>
       <Button type="submit" className={`${signInBtnStyles} mt-4`}>
-        {action === "Login" ? "Sign in" : "Sign up"}
+        {action === formActions.logIn ? formActions.signIn : formActions.signUp}
       </Button>
       {children}
     </form>
