a450374f4d45fbe755fa06b11a9fdf7fbb393187
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-03-27 14:02:02 +0100

    feat: Add sign up functionality

--- features/signup-form/SignUpForm.tsx
+++ features/signup-form/SignUpForm.tsx
@@ -1,14 +1,38 @@
 import Form from "@components/form/Form";
 import HasAccount from "@components/login/HasAccount";
+import useAuth from "@hooks/useAuth";
+import { authActions } from "@store/actions";
+import { formActions } from "domain/constants";
+import { UserDetails } from "domain/types";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
-export default function SignUpForm({ onLogin }: { onLogin: () => void }) {
+export default function SignUpForm({
+  onLogin,
+  onModalClose,
+}: {
+  onLogin: () => void;
+  onModalClose: () => void;
+}) {
+  const { handleUser } = useAuth(createUserWithEmailAndPassword, authActions.login);
+
+  const [userDetails, setUserDetails] = useState<UserDetails>({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log("asdasd");
+    handleUser(userDetails.email, userDetails.password);
+    onModalClose();
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form
+      action={formActions.signUp}
+      onSubmit={handleSubmit}
+      userDetails={userDetails}
+      onHandleUserDetails={setUserDetails}>
       <HasAccount action="Login" onLogin={onLogin}>
         Already have an account ?
       </HasAccount>
