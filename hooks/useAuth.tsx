4c521fdaa42c9b6deca29ce72d3a68bf9ad5ad32
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-14 22:38:37 +0100

    feat: Add loading state and error handling

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,5 +1,8 @@
 import { useAuthContext } from "@context/AuthContext";
+import { handleAuthError } from "@helpers/helpers";
 import { auth } from "@lib/config";
+import { AuthErrorMessage } from "domain/types";
+import { FirebaseError } from "firebase/app";
 import { Auth, User, UserCredential } from "firebase/auth";
 import { useState } from "react";
 
@@ -8,19 +11,26 @@ export default function useAuth(
   action: string
 ) {
   const { dispatch } = useAuthContext();
-  // error and loading state !!!
   const [user, setUser] = useState<null | User>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<null | AuthErrorMessage>(null);
 
   async function handleUser(email: string, password: string) {
+    setLoading(true);
+    setError(null);
+
     try {
       const userCredential = await userAction(auth, email, password);
       setUser(userCredential.user);
       dispatch({ type: action, payload: userCredential.user });
-    } catch {
-      // sort it out
-      console.log("123");
+      setLoading(false);
+    } catch (error: unknown) {
+      if (error instanceof FirebaseError) {
+        setError(handleAuthError(error));
+        setLoading(false);
+      }
     }
   }
 
-  return { handleUser, user };
+  return { handleUser, user, loading, error };
 }
