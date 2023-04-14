95493d675ae10cfb91d2cb54ec8798fe98d1d366
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-14 22:42:04 +0100

    chore: Implement greeting message after sign in

--- components/user-menu/UserMenu.tsx
+++ components/user-menu/UserMenu.tsx
@@ -1,33 +1,40 @@
 import Button from "@components/button/Button";
 import Avatar from "@components/ui/avatar/Avatar";
 import ChevronIcon from "@components/ui/icons/ChevronIcon";
+import Toast from "@components/ui/toast/Toast";
 import UserMenuOptions from "@components/user-menu-options/UserMenuOptions";
 import { useAuthContext } from "@context/AuthContext";
 import useClickOutside from "@hooks/useClickOutside";
 import useToggle from "@hooks/useToggle";
 
 export default function UserMenu() {
-  const { isShown, handleOnShow } = useToggle();
-  const { userName = "" } = useAuthContext();
-  const showUserMenu = isShown && <UserMenuOptions />;
+  const { isShown: showUserMenuDropdown, handleOnShow: handleUserMenuDropdown } = useToggle();
+  const { user, userName = "" } = useAuthContext();
 
-  function toggleProfileMenu() {
-    handleOnShow(!isShown);
-  }
+  const showProfileMenu = showUserMenuDropdown && <UserMenuOptions />;
 
-  const domNode = useClickOutside(toggleProfileMenu);
+  const activeDomNode = useClickOutside(() => handleUserMenuDropdown(false));
+
+  const showUserGreeting = user ? (
+    <Toast position="top-24">
+      <span className="text-2xl text-center">Welcome {userName} !</span>
+    </Toast>
+  ) : (
+    ""
+  );
 
   return (
     <>
-      <div ref={domNode} className="hidden min-w-[10rem] lg:flex mr-4 ml-auto">
+      {showUserGreeting}
+      <div ref={activeDomNode} className="hidden min-w-[10rem] lg:flex mr-4 ml-auto">
         <div className="w-full flex items-center justify-between relative cursor-pointer">
           <div className="rounded-full">
-            {showUserMenu}
+            {showProfileMenu}
             <div className="mx-3">
               <Avatar src="file" name={userName} />
             </div>
           </div>
-          <Button onClick={toggleProfileMenu}>
+          <Button onClick={() => handleUserMenuDropdown(!showUserMenuDropdown)}>
             <ChevronIcon size={20} fill="#f1b24a" />
           </Button>
         </div>
