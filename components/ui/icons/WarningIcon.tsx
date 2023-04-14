9b6c684538e769daf9b4e5b0414a4ce070ca41a0
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-14 22:43:21 +0100

    chore: Add warning icon

---
+++ components/ui/icons/WarningIcon.tsx
@@ -0,0 +1,17 @@
+export default function WarningIcon() {
+  return (
+    <svg
+      aria-hidden="true"
+      className="mx-auto mb-4 w-24 h-24"
+      fill="none"
+      stroke="#f44336"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
+  );
+}
