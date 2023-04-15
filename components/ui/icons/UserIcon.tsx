fd3e54dbcd2194c6ffa4b6fbe6ab934c50066b58
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-15 22:16:30 +0100

    chore: make component accessible

--- components/ui/icons/UserIcon.tsx
+++ components/ui/icons/UserIcon.tsx
@@ -6,6 +6,8 @@ type UserIconProps = {
 export default function UserIcon({ fill, size }: UserIconProps) {
   return (
     <svg
+      role="img"
+      aria-labelledby="sr-only"
       width={size}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 25 25"
@@ -17,6 +19,7 @@ export default function UserIcon({ fill, size }: UserIconProps) {
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z"
         clipRule="evenodd"
       />
+      <title id="sr-only">Click to open mobile menu</title>
     </svg>
   );
 }
