8ab46184946e0be453b8858bc3e937bee27e057f
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-15 22:14:17 +0100

    refactor: Move logic from Login and SignUpForm down

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -3,23 +3,31 @@ import ClearInputFieldIcon from "@components/ui/icons/ClearInputFieldIcon";
 import HidePasswordIcon from "@components/ui/icons/HidePasswordIcon";
 import ShowPasswordIcon from "@components/ui/icons/ShowPasswordIcon";
 import Input from "@components/ui/input-field/Input";
+import Spinner from "@components/ui/spinner/Spinner";
+import useAuth from "@hooks/useAuth";
 import { formActions, loginBtnStyles, signInBtnStyles } from "domain/constants";
-import { FormProps } from "domain/types";
+import { FormProps, UserDetails } from "domain/types";
 import { useState } from "react";
 
-export default function Form({
-  children,
-  action,
-  onSubmit,
-  userDetails,
-  onHandleUserDetails,
-}: FormProps) {
+export default function Form({ children, action, method, onModalClose }: FormProps) {
+  const { handleUser, loading, error } = useAuth(method, action);
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [userDetails, setUserDetails] = useState<UserDetails>({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleUser(userDetails.email, userDetails.password);
+
+    if (error !== null) onModalClose();
+  }
 
   function handleUserDetails(e: Event) {
     const { name, value } = e.target as HTMLInputElement;
 
-    onHandleUserDetails({
+    setUserDetails({
       ...userDetails,
       [name]: value,
     });
@@ -30,51 +38,66 @@ export default function Form({
   ) : (
     <ShowPasswordIcon size={28} />
   );
-console.log(userDetails)
+
   return (
-    <form onSubmit={onSubmit} className="relative space-y-6">
-      <div className="space-y-1 text-sm">
-        <Input
-          onChange={handleUserDetails}
-          value={userDetails.email}
-          srOnly={false}
-          type="email"
-          name="email"
-          id="userName"
-          placeholder="john@gmail.com"
-          className={`${loginBtnStyles}`}
-          required={true}>
-          E-mail
-        </Input>
-      </div>
-      <div className="relative pace-y-1 text-sm">
-        <Input
-          onChange={handleUserDetails}
-          value={userDetails.password}
-          srOnly={false}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="*************"
-          className={`${loginBtnStyles}`}
-          required={true}>
-          Password
-        </Input>
-        <Button
-          className="absolute top-[1.6rem] right-[.9rem] z-50"
-          onClick={() => setShowPassword(!showPassword)}>
-          {isPasswordVisible}
-        </Button>
-        {userDetails.password.length > 0 && (
-          <Button onClick={() => onHandleUserDetails({ ...userDetails, password: "" })}>
-            <ClearInputFieldIcon className="absolute top-[1.7rem] right-[2.6rem] z-50" size={25} />
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <form onSubmit={handleSubmit} className="relative space-y-6">
+          {/* // add validation css */}
+          <p>{error?.auth}</p>
+          <div className="space-y-1 text-sm">
+            <Input
+              onChange={handleUserDetails}
+              value={userDetails.email.trim()}
+              srOnly={false}
+              type="email"
+              name="email"
+              id="userName"
+              placeholder="john@gmail.com"
+              className={`${loginBtnStyles}`}
+              required={true}>
+              E-mail
+            </Input>
+            {/* // add validation css */}
+            <p>{error?.email}</p>
+          </div>
+          <div className="relative pace-y-1 text-sm">
+            <Input
+              onChange={handleUserDetails}
+              value={userDetails.password.trim()}
+              srOnly={false}
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="*************"
+              className={`${loginBtnStyles}`}
+              required={true}>
+              Password
+            </Input>
+            <Button
+              className="absolute top-[1.6rem] right-[.9rem] z-50"
+              onClick={() => setShowPassword(!showPassword)}>
+              {isPasswordVisible}
+            </Button>
+            {userDetails.password.length > 0 && (
+              <Button onClick={() => setUserDetails({ ...userDetails, password: "" })}>
+                <ClearInputFieldIcon
+                  className="absolute top-[1.7rem] right-[2.6rem] z-50"
+                  size={25}
+                />
+              </Button>
+            )}
+            {/* // add validation css */}
+            <p>{error?.password}</p>
+          </div>
+          <Button type="submit" className={`${signInBtnStyles} mt-4`}>
+            {action === formActions.signIn ? formActions.signIn : formActions.signUp}
           </Button>
-        )}
-      </div>
-      <Button type="submit" className={`${signInBtnStyles} mt-4`}>
-        {action === formActions.logIn ? formActions.signIn : formActions.signUp}
-      </Button>
-      {children}
-    </form>
+          {children}
+        </form>
+      )}
+    </>
   );
 }
