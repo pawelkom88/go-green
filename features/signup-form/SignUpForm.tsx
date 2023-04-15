c78364c2f637a279dfd485798486d03b82730a15
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-15 22:13:55 +0100

    refactor: Refactor both components and remove shared logic

--- features/signup-form/SignUpForm.tsx
+++ features/signup-form/SignUpForm.tsx
@@ -1,11 +1,7 @@
 import Form from "@components/form/Form";
 import HasAccount from "@components/login/HasAccount";
-import useAuth from "@hooks/useAuth";
-import { authActions } from "@store/actions";
 import { formActions } from "domain/constants";
-import { UserDetails } from "domain/types";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
 
 export default function SignUpForm({
   onLogin,
@@ -14,28 +10,17 @@ export default function SignUpForm({
   onLogin: () => void;
   onModalClose: () => void;
 }) {
-  const { handleUser } = useAuth(createUserWithEmailAndPassword, authActions.login);
-
-  const [userDetails, setUserDetails] = useState<UserDetails>({
-    email: "",
-    password: "",
-  });
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    handleUser(userDetails.email, userDetails.password);
-    onModalClose();
-  }
-
   return (
-    <Form
-      action={formActions.signUp}
-      onSubmit={handleSubmit}
-      userDetails={userDetails}
-      onHandleUserDetails={setUserDetails}>
-      <HasAccount action="Login" onLogin={onLogin}>
-        Already have an account ?
-      </HasAccount>
-    </Form>
+    <>
+      <Form
+        method={createUserWithEmailAndPassword}
+        action={formActions.signUp}
+        onModalClose={onModalClose}>
+        <HasAccount action="Login" onLogin={onLogin}>
+          Already have an account ?
+        </HasAccount>
+      </Form>
+      {/* <ErrorMessage error={error} /> */}
+    </>
   );
 }
