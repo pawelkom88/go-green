3e930f39c4653f3e4d19f93279239d4ce2028853
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-16 23:00:12 +0100

    feat: Add warning message when log out

--- components/user-menu-options/UserMenuOptions.tsx
+++ components/user-menu-options/UserMenuOptions.tsx
@@ -2,38 +2,73 @@ import Button from "@components/button/Button";
 import Modal from "@components/modal/Modal";
 import ProfileIcon from "@components/ui/icons/ProfileIcon";
 import SignOutIcon from "@components/ui/icons/SignOutIcon";
+import Spinner from "@components/ui/spinner/Spinner";
+import Toast from "@components/ui/toast/Toast";
 import UserProfile from "@features/user-profile/UserProfile";
-import useToggle from "@hooks/useToggle";
+import useLogout from "@hooks/useLogout";
+import { LogOutMetrics } from "domain/types";
+import { useState } from "react";
+import LogOutMessagePopUp from "./LogOutMessagePopUp";
 
 export default function UserMenuOptions() {
-  const { isShown, handleOnShow } = useToggle();
+  const { logUserOut, loading, error } = useLogout();
+  const [manageLogOut, setManageLogOut] = useState<LogOutMetrics>({
+    openModal: false,
+    showLogOutPopUpMessage: false,
+  });
 
-  const showUserProfile = isShown && (
-    <Modal size="w-full h-3/4" onModalClose={() => handleOnShow(false)}>
+  function handleLogOut() {
+    logUserOut();
+  }
+
+  const showUserProfile = manageLogOut.openModal && (
+    <Modal
+      size="w-full h-3/4"
+      onModalClose={() => setManageLogOut({ ...manageLogOut, openModal: false })}>
       <UserProfile />
     </Modal>
   );
 
-  function signOut() {
-    console.log("asd");
-  }
+  const showLogOutWarningMessage = manageLogOut.showLogOutPopUpMessage && (
+    <Modal
+      size="w-full h-1/3"
+      onModalClose={() =>
+        setManageLogOut(manageLogOutState => ({
+          ...manageLogOutState,
+          showLogOutPopUpMessage: false,
+        }))
+      }>
+      <LogOutMessagePopUp onHandleLogOut={handleLogOut} onManageLogOut={setManageLogOut} />
+    </Modal>
+  );
+
+
 
   return (
     <>
-      <ul className="p-4 w-full border-r bg-primary-clr absolute rounded left-0 shadow mt-12 sm:mt-16">
-        <li className="flex w-full justify-between hover:text-secondary-clr cursor-pointer items-center">
-          <Button onClick={() => handleOnShow(true)} className="text-md flex">
-            <ProfileIcon className="mr-[5px]" size={25} fill="none" />
-            My Profile
-          </Button>
-        </li>
-        <li className="flex w-full justify-between hover:text-secondary-clr  cursor-pointer items-center mt-2">
-          <Button onClick={signOut} className="text-md flex">
-            <SignOutIcon className="mr-[5px]" size={25} fill="none" />
-            Sign out
-          </Button>
-        </li>
-      </ul>
+      {showLogOutWarningMessage}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <ul className="p-4 w-full border-r bg-primary-clr absolute rounded left-0 shadow mt-12 sm:mt-16">
+          <li className="flex w-full justify-between hover:text-secondary-clr cursor-pointer items-center">
+            <Button
+              onClick={() => setManageLogOut({ ...manageLogOut, openModal: true })}
+              className="text-md flex">
+              <ProfileIcon className="mr-[5px]" size={25} fill="none" />
+              My Profile
+            </Button>
+          </li>
+          <li className="flex w-full justify-between hover:text-secondary-clr  cursor-pointer items-center mt-2">
+            <Button
+              onClick={() => setManageLogOut({ ...manageLogOut, showLogOutPopUpMessage: true })}
+              className="text-md flex">
+              <SignOutIcon className="mr-[5px]" size={25} fill="none" />
+              Sign out
+            </Button>
+          </li>
+        </ul>
+      )}
       {showUserProfile}
     </>
   );
