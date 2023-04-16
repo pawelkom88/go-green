b442d83aa06deda1375a315a6bef98fc2013c97b
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-16 22:50:48 +0100

    refactor: Refactor logic in UserCredentialModal

--- components/login/login-modal/UserCredentialModal.tsx
+++ components/login/login-modal/UserCredentialModal.tsx
@@ -1,50 +1,36 @@
-import Button from "@components/button/Button";
-import Modal from "@components/modal/Modal";
 import AnimatedLogo from "@components/ui/icons/AnimatedLogo";
 import LoginForm from "@features/login-form/LoginForm";
 import SignUpForm from "@features/signup-form/SignUpForm";
 import useToggle from "@hooks/useToggle";
-import { formActions, signInBtnStyles } from "domain/constants";
+import { formActions } from "domain/constants";
+import { GenericFunction } from "domain/types";
 
-export default function UserCredentialModal() {
-  const { isShown: showUserCredentialModal, handleOnShow: handleShowUserCredentialModal } =
-    useToggle();
+export default function UserCredentialModal({
+  onHandleShowUserCredentialModal,
+}: {
+  onHandleShowUserCredentialModal: GenericFunction<boolean>;
+}) {
   const { isShown: isRegistered, handleOnShow: handleIsRegistered } = useToggle();
 
   const registrationForm = isRegistered ? (
     <SignUpForm
-      onModalClose={() => handleShowUserCredentialModal(false)}
+      onModalClose={onHandleShowUserCredentialModal}
       onLogin={() => handleIsRegistered(false)}
     />
   ) : (
     <LoginForm
-      onModalClose={() => handleShowUserCredentialModal(false)}
+      onModalClose={onHandleShowUserCredentialModal}
       onSignedUp={() => handleIsRegistered(true)}
     />
   );
 
-  const showLoginModal = showUserCredentialModal && (
-    <Modal
-      size="w-full h-2/3 flex-center"
-      onModalClose={() => handleShowUserCredentialModal(false)}>
-      <div className="w-full max-w-sm p-8 space-y-3 rounded-xl  text-dark-text-clr -mt-24">
-        <AnimatedLogo />
-        <h1 className="text-2xl font-bold text-center">
-          {isRegistered ? formActions.signUp : formActions.logIn}
-        </h1>
-        {registrationForm}
-      </div>
-    </Modal>
-  );
-
   return (
-    <>
-      <Button
-        onClick={() => handleShowUserCredentialModal(true)}
-        className={`${signInBtnStyles} hover:bg-white hover:text-primary-clr mr-4 ml-auto hidden lg:block`}>
-        LOG IN
-      </Button>
-      {showLoginModal}
-    </>
+    <div className="w-full max-w-sm p-8 space-y-3 rounded-xl  text-dark-text-clr -mt-24">
+      <AnimatedLogo />
+      <h1 className="text-2xl font-bold text-center">
+        {isRegistered ? formActions.signUp : formActions.logIn}
+      </h1>
+      {registrationForm}
+    </div>
   );
 }
