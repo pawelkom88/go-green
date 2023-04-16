ba4b4081107cd6d93f14dbe13c54bec28c45de69
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-16 22:58:44 +0100

    fix: Fix auth error logic

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,10 +1,12 @@
 import { useAuthContext } from "@context/AuthContext";
 import { handleAuthError } from "@helpers/helpers";
 import { auth } from "@lib/config";
-import { AuthErrorMessage } from "domain/types";
+import { AuthError } from "domain/types";
 import { FirebaseError } from "firebase/app";
 import { Auth, User, UserCredential } from "firebase/auth";
 import { useState } from "react";
+
+const defaultAuthErrorState: AuthError = { occured: true, message: "" };
 
 export default function useAuth(
   userAction: (auth: Auth, email: string, password: string) => Promise<UserCredential>,
@@ -13,20 +15,21 @@ export default function useAuth(
   const { dispatch } = useAuthContext();
   const [user, setUser] = useState<null | User>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<null | AuthErrorMessage>(null);
+  const [error, setError] = useState(defaultAuthErrorState);
 
   async function handleUser(email: string, password: string) {
     setLoading(true);
-    setError(null);
+    setError({ occured: false, message: "" });
 
     try {
       const userCredential = await userAction(auth, email, password);
       setUser(userCredential.user);
       dispatch({ type: action, payload: userCredential.user });
       setLoading(false);
+      setError({ occured: false, message: "" });
     } catch (error: unknown) {
       if (error instanceof FirebaseError) {
-        setError(handleAuthError(error));
+        setError({ occured: true, message: handleAuthError(error) });
         setLoading(false);
       }
     }
