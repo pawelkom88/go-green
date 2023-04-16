936775bf768826bfaa19cefdfe4d1ad6e24452c2
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-04-16 22:51:30 +0100

    styles: tweak styles

--- components/modal/Overlay.tsx
+++ components/modal/Overlay.tsx
@@ -1,8 +1,10 @@
 import { OverlayType } from "domain/types";
 
-export default function Overlay({ children, onClose }: OverlayType) {
+export default function Overlay({ children, onClose, blur = "blur-none" }: OverlayType) {
   return (
-    <div onClick={onClose} className="fixed inset-0 backdrop-brightness-50 bg-opacity-75 z-50 flex-center">
+    <div
+      onClick={onClose}
+      className={`${blur} fixed inset-0 backdrop-brightness-50 bg-opacity-75 z-50 flex-center`}>
       {children}
     </div>
   );
